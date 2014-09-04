af6e9ccf21f0dbed488bdd3a96320ccdd64d2570
Adrien Le Menthéour <adrien.le.mentheour@gmail.com>
2014-09-04 22:42:56 +0200

    Gifs projets

--- js/script.js
+++ js/script.js
@@ -6,25 +6,13 @@ var animationEase = Cubic.easeInOut;
 var animationTime = 1;
 
 $(document).ready(function(){
-	// Simple way to attach js code to the canvas is by using a function
-	//function sketchProc(pjs) {
-		// mode colorimetrique
-	//	pjs.colorMode(pjs.RGB);
-		// taille du canvas
-	//	pjs.size($("#bloc-illus").width(), $("#bloc-illus").height(), pjs.WEBGL);
-			
-	//	};
-		
-	//}
-	
-	//var canvas = document.getElementById("canvas-bloc-illus");
-	//var p = new Processing(canvas, sketchProc);
+
 
 	var canvas = document.getElementById('canvas-bloc-illus');
 	// Create an empty project and a view for the canvas:
 	paper.setup(canvas);
 	var tool = new paper.Tool();
-	
+	tool.maxDistance = 10;
 	//paper.setViewSize($("#bloc-illus").width(), $("#bloc-illus").width());
 	
 	// Create a Paper.js Path to draw a line into it:
@@ -34,6 +22,7 @@ $(document).ready(function(){
 	myCircle.strokeColor = 'black';
 	myCircle.selected = true;*/
 	var myCircle = new paper.Path.Circle(new paper.Point(centreX, centreY), 150);
+	myCircle.rasterize(300);
 	var jauneFolioTransparent = new paper.RgbColor(250, 249, 219, 0);
 	myCircle.fillColor = {
 	    gradient: {
@@ -41,44 +30,47 @@ $(document).ready(function(){
 	        radial: true
 	    },
 	    origin: myCircle.position,
-	    destination: myCircle.bounds.rightCenter
+	    destination: myCircle.bounds.height
 	};
-	tool.onFrame = function(event) {
-		console.log("aze");
+	myCircle.onFrame = function(event) {
+		if(myCircle.bounds.height>myCircle.bounds.width){
+			myCircle.fillColor = {
+			    gradient: {
+			        stops: [['#1e32d2', 0], [jauneFolioTransparent, 1]],
+			        radial: true
+			    },
+			    origin: myCircle.position,
+			    destination: myCircle.bounds.width
+			};
+		}else {
+			myCircle.fillColor = {
+			    gradient: {
+			        stops: [['#1e32d2', 0], [jauneFolioTransparent, 1]],
+			        radial: true
+			    },
+			    origin: myCircle.position,
+			    destination: myCircle.bounds.height
+			};
+		}
 	}
 	tool.onMouseMove = function(event) {
 		var location = myCircle.getNearestLocation(event.point);
 		var segment = location.segment;
 		var point = segment.point;
 		console.log("x : "+point.x+" y : "+point.y);
-		if (location.distance < 20) {
-			var y = event.point.y;
-			point.y += (y - point.y) / 6;
-			var x = event.point.x;
-			point.x += (x - point.x) / 6;
-		}
 		
-		/*if (!point.fixed && location.distance < 20) {
+		if (location.distance < 60) {
 			var y = event.point.y;
-			point.y += (y - point.y) / 6;
+			point.y += (y - point.y) / 3;
 			var x = event.point.x;
-			point.x += (x - point.x) / 6;
-			if (segment.previous && !segment.previous.fixed) {
-				var previous = segment.previous.point;
-				previous.y += (y - previous.y) / 48;
-				previous.x += (x - previous.x) / 48;
-			}
-			if (segment.next && !segment.next.fixed) {
-				var next = segment.next.point;
-				next.y += (y - next.y) / 48;
-				next.x += (x - next.x) / 48;
-			}
-		}*/
+			point.x += (x - point.x) / 3;
+		}
 	}
 	
-	// selectionné
+	
 	
 	tool.onMouseDown= function(event) {
+		// selectionné
         myCircle.selected = true;
     }
     
